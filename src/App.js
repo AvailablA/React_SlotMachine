37874ef3d19bb4e896c56a0c1611f0ffba9cd65c
AvailablA <manohar.shewale77@gmail.com>
2023-09-22 12:19:45 +0530

    The slot machine demo14 add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SlotM = (props) =>
+{
+  let x = props.x;
+  let y = props.y;
+  let z = props.z;
+
+  if((x===y)&&(x===z))
+  {
+    return(
+      <>
+        <div className='slot_in'>
+          <h1>
+            {" "}
+            {x} {y} {z}
+          </h1>
+          <h1>This is matching</h1>
+        </div>
+      </>
+    )
+  }
+  else
+  {
+    return(
+      <>
+        <div className='slot_in'>
+          <h1>
+            {" "}
+            {x} {y} {z}
+          </h1>
+          <h1>This is Not matching</h1>
+        </div>
+      </>
+    )
+
+  }
 }
 
+const App = () =>
+{
+  return <>
+    
+    <h1 className='heading'> 🎰 Welcome to <span className='head'>SLOT Machine 🎰</span></h1>
+
+    <div className='slot_machine'>
+      <hr/>
+      <SlotM x="😀" y="😀" z="😀"/>
+      <hr/>
+      <SlotM x="😀" y="😁" z="🤩"/>
+      <hr/>
+      <SlotM x="🥶" y="🤯" z="😭"/>
+      <hr/>
+    </div>
+    </>
+}
 export default App;
